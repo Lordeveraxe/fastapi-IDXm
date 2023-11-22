1d109c0ddcc42bc12edba7275044717169db56b0
Andres Torres <santiago.ojedatorres@gmail.com>
2023-11-22 01:46:22 -0500

    Update model download and prediction code

--- main.py
+++ main.py
@@ -7,28 +7,38 @@ import numpy as np
 
 app = FastAPI()
 
-# URL del modelo
-model_url = "https://drive.google.com/uc?id=1wMb03-UkWY2PmWkvZKUxXZppuINfOFza"
+# URL de Google Drive para el archivo compartido
+file_id = "1wMb03-UkWY2PmWkvZKUxXZppuINfOFza"
+url = f"https://drive.google.com/uc?id={file_id}"
 
 # Descargar el modelo
-response = requests.get(model_url)
-model_file = BytesIO(response.content)
+response = requests.get(url)
+with open("modelo_temporal.h5", "wb") as file:
+    file.write(response.content)
 
-# Cargar el modelo
-model = load_model(model_file)
+model = load_model("modelo_temporal.h5")
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
 
 @app.post("/predict/")
-async def predict(file: UploadFile = File(...)):
-    # Convertir el archivo cargado en una imagen
+async def create_upload_file(file: UploadFile = File(...)):
+    # Lee y procesa la imagen
     image = Image.open(BytesIO(await file.read()))
-    image = image.resize((224, 224)) # Asegúrate de cambiar el tamaño según tu modelo
-    image = np.expand_dims(image, axis=0)
+    image = image.resize((100, 100)) # Asegúrate de cambiar el tamaño según tu modelo
+    image_array = np.array(image)
 
-    # Realizar predicción
-    prediction = model.predict(image)
+    # Realiza la predicción
+    prediction = model.predict(np.array([image_array]))
+    predicted_class_index = np.argmax(prediction, axis=1)[0]
 
-    return {"prediction": prediction.tolist()}
+    # Diccionario de vegetales con sus respectivos índices
+    vegetables = ['Bean', 'Bitter_Gourd', 'Bottle_Gourd', 'Brinjal', 'Broccoli', 
+                  'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Cucumber', 
+                  'Papaya', 'Potato', 'Pumpkin', 'Radish', 'Tomato']
+
+    # Obtiene el nombre del vegetal predicho
+    predicted_vegetable = vegetables[predicted_class_index]
+
+    return {"predicted_vegetable": predicted_vegetable, "prediction": prediction.tolist()}
