d2fc06fed572e4a9544eeed93ba2c024185de2e7
Andres Torres <santiago.ojedatorres@gmail.com>
2023-11-22 01:52:53 -0500

    Changes

--- main.py
+++ main.py
@@ -42,3 +42,6 @@ async def create_upload_file(file: UploadFile = File(...)):
     predicted_vegetable = vegetables[predicted_class_index]
 
     return {"predicted_vegetable": predicted_vegetable, "prediction": prediction.tolist()}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
